953203c0534e9b60e34469294a0ad4418c2dbc8d
anyaa07 <128918201+anyaa07@users.noreply.github.com>
2023-04-04 14:26:24 -0400

    Update untitled0.py

--- untitled0.py
+++ untitled0.py
@@ -74,7 +74,7 @@ intp = (data1['INT_2023_24'] / data1['Att_2023_24']) * 0.25
 data1['QBR'] = ((data1['Comp_2023_24'] - 30) / 20 + ((data1['Pass Yds_2023_24'] / data1['Att_2023_24']) - 3) * 0.25 + (data1['TD_2023_24']) * 0.2 + 2.375 - (data1['INT_2023_24'] * 0.25)) * 100 / 6 / 3
 
 selected_columns = data1[['Team', 'Player', 'Pass Yds_2023_24', 'TD_2023_24', 'INT_2023_24', 'Comp_2023_24', 'Att_2023_24', 'QBR', 'Fantasy_Points']]
-st.write("Hello! Welcome to the Predict application. Here you can input the name of the quarterback to see what stats that quarterback is predicted to have for the 2023-2024 season!")
+st.write("Hello! Welcome to the Predict application. Here you can input the name of a quarterback to see what stats that quarterback is predicted to have for the 2023-2024 season!")
 player_search = st.text_input("Enter the name of the quarterback you want to search for (ensure that the name is capitalized and spelled correctly):")
 
 # filter data for player search
